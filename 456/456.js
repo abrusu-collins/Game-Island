536d2324fcf02b003b1fbc45ef85e90042f1a2da
abrusu-collins <snillochemingson@gmail.com>
2022-07-11 22:11:06 +0000

    It's coming together very well

--- 456/456.js
+++ 456/456.js
@@ -2,7 +2,7 @@ let num= document.getElementById("num");
 
 let numArray =["4","5","6"];
 
-
+//shuffle function
 function shuffle(array) {
     for (i = 0; i <= array.length - 1; i++) {
       let randonNumber = Math.floor(Math.random() * (i+1));
@@ -14,9 +14,26 @@ function shuffle(array) {
   }
 
 
-
+// numchange function
   function numchange(){
    let newArray= shuffle(numArray);
-   let numtodisplay= parseInt(newArray[0]);
+   let numtodisplay= newArray[0];
    let numcount= parseInt(newArray[1]);
+   let elementtodisplay= "";
+   for(let i=1; i<=numcount; i++){
+    elementtodisplay += numtodisplay;
+   }
+   window.addEventListener("keydown",(e)=>{
+    if (parseInt(String.fromCharCode(e.keyCode)) == numcount){
+        numchange();
+    }
+    else{
+        
+    };
+   })
+   console.log(numtodisplay);
+   console.log(numcount);
+   console.log(elementtodisplay)
   }
+
+  numchange();
