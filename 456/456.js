179e44a08d7687f368ddd5cdd9afe479bdadf4de
abrusu-collins <snillochemingson@gmail.com>
2022-07-12 16:20:31 +0000

    from scratch

--- 456/456.js
+++ 456/456.js
@@ -14,31 +14,4 @@ function shuffle(array) {
     return array;
   }
 
-
-// numchange function
-  function numchange(){
-   let newArray= shuffle(numArray);
-   let numtodisplay= newArray[0];
-   let numcount= parseInt(newArray[1]);
-   let elementtodisplay= "";
-   for(let i=1; i<=numcount; i++){
-    elementtodisplay += numtodisplay;
-   }
-   window.addEventListener("keydown",(e)=>{
-    if (parseInt(String.fromCharCode(e.keyCode)) === numcount){
-console.log(typeof(parseInt(String.fromCharCode(e.keyCode))))       
-        numchange();
-        score=score +1;
-
-    }
-    else{
-       console.log(" window.removeEventListener(keydown);")
-    };
-   })
-   console.log(numtodisplay);
-   console.log(numcount);
-   console.log(elementtodisplay)
-   console.log(score)
-  };
-
-  numchange();
+// function numchange
