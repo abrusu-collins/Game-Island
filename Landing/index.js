f16b29eae561c5db9f21d2bd5e3e84494f15028a
abrusu-collins <snillochemingson@gmail.com>
2022-07-17 20:32:43 +0000

    started working on games section

--- Landing/index.js
+++ Landing/index.js
@@ -3,7 +3,7 @@ var navbar = document.querySelector('.nav')
 window.onscroll = function() {
 
   // pageYOffset or scrollY
-  if (window.pageYOffset > 600 ) {
+  if (window.pageYOffset > 100 ) {
     navbar.classList.add('scrolled')
   } else {
     navbar.classList.remove('scrolled')
