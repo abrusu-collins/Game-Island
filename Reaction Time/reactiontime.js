aa0a8b637b8d2b0526fd44f1aa4f42ab95636b5f
abrusu-collins <snillochemingson@gmail.com>
2022-07-19 16:15:00 +0000

    fixed bug

--- Reaction Time/reactiontime.js
+++ Reaction Time/reactiontime.js
@@ -1,6 +1,5 @@
-let score=0;
-let scoreinterval
 let greenclick;
+let count=0;
 let start = document.getElementById("start");
 let red=`<div class="red" id="red">
 <p>Wait for green</p>
@@ -17,27 +16,34 @@ function initiate(e){
     e.preventDefault();
     document.body.innerHTML=red;
     document.getElementById("red").addEventListener("click",toosoon,true);
+ 
     setTimeout(()=>{
+        if(count===0){
         document.body.innerHTML=green;
-         scoreinterval=setInterval(()=>{score++},1);
          greenclick=document.getElementById("green");
         greenclick.addEventListener("click",displayscore,true);
-    },7000)
+        firstDate= +Date.now();
+    }
+    else{}
+    },3000)
+   
 };
 
 function displayscore(e){
     e.preventDefault();
-    clearInterval(scoreinterval);
+ 
     let restart =` <div class="restart">
-<h1>${score}ms</h1>
+<h1>${Math.abs((+Date.now() - firstDate))}ms</h1>
 <a href="">Restart</a>
 </div>`;
     document.body.innerHTML=restart;
-    console.log(score);
+    console.log(Math.abs((+Date.now() - firstDate)));
+    
 }
 
 function toosoon(e){
     e.preventDefault();
+    count=count+1;
     let toosoondiv=` <div class="restart">
     <h1>Too Soon</h1>
     <a href="">Restart</a>
