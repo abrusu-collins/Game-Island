3eb99b2d9d82fe68f827c6226b5b19a90c98ad62
abrusu-collins <snillochemingson@gmail.com>
2022-07-21 23:27:23 +0000

    started working on js

---
+++ number guess/numberguess.js
@@ -0,0 +1,4 @@
+let input =  document.getElementById("numberinput");
+let check = document.getElementById("check");
+let scorediv = document.getElementById("score");
+let guide = document.getElementById("guides");
