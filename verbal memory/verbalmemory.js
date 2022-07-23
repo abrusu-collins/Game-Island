df9a9ad3292f67f60e2e57ec0c681c06c2f38659
abrusu-collins <snillochemingson@gmail.com>
2022-07-23 16:50:38 +0000

    Start button event

--- verbal memory/verbalmemory.js
+++ verbal memory/verbalmemory.js
@@ -5,3 +5,19 @@ let score = document.getElementById("score");
 let word = document.getElementById("word");
 let seenword = document.getElementById("seen");
 let newword = document.getElementById("new");
+let wordArray=[];
+
+
+start.addEventListener("click",fetchwords,true);
+
+function fetchwords(e){
+    e.preventDefault();
+    fetch("https://random-word-api.herokuapp.com/word?number=100")
+    .then((response)=>{
+        return response.json()
+    })
+    .then((data)=>{
+        wordArray=data;
+        console.log(wordArray);
+    })
+}
