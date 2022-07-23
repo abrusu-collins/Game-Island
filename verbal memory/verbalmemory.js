9ae75b3cbd5cdce409b8d72be1696be471738e40
abrusu-collins <snillochemingson@gmail.com>
2022-07-23 16:34:35 +0000

    JS underway

---
+++ verbal memory/verbalmemory.js
@@ -0,0 +1,7 @@
+
+let start = document.getElementById("start");
+let life = document.getElementById("life");
+let score = document.getElementById("score");
+let word = document.getElementById("word");
+let seenword = document.getElementById("seen");
+let newword = document.getElementById("new");
