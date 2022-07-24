f8109f59958ca576e5d1fea1c67e01aee4a6a36d
abrusu-collins <snillochemingson@gmail.com>
2022-07-24 12:36:48 +0000

    added spinner

--- verbal memory/verbalmemory.js
+++ verbal memory/verbalmemory.js
@@ -2,7 +2,7 @@ let start = document.getElementById("start");
 let life = document.getElementById("life");
 let score = document.getElementById("score");
 let word = document.getElementById("word");
-let seenbuton = document.getElementById("seen");
+let seenbutton = document.getElementById("seen");
 let newbutton = document.getElementById("new");
 let wordArray = [];
 let seenwordArray = [];
@@ -19,9 +19,12 @@ function shuffle(array) {
 }
 
 start.addEventListener("click", fetchwords, true);
+seenbutton.addEventListener("click",checkseen,true);
+newbutton.addEventListener("click",checknew,true);
 
 function fetchwords(e) {
   e.preventDefault();
+  word.innerHTML=`<h1> <i class="fa fa-spinner fa-spin"></i> </h1>`
   fetch("https://random-word-api.herokuapp.com/word?number=100")
     .then((response) => {
       return response.json();
@@ -39,4 +42,19 @@ function fetchwords(e) {
 function displaywords() {
   shuffle(wordArray);
   word.innerHTML = `<h1>${wordArray[0]}</h1>`;
+}
+
+function checkseen(e){
+  e.preventDefault();
+}
+
+function checknew(e){
+  e.preventDefault();
+  if(!(seenwordArray.includes(word.textContent))){
+    seenwordArray.push(word.textContent);
+  console.log(seenwordArray);
+  displaywords();
+  }
+  
+
 }
