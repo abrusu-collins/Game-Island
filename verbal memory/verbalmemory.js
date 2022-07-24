61a8c4fa4b2a9a8c0c0ef7f3879aaad88ac96cff
abrusu-collins <snillochemingson@gmail.com>
2022-07-24 02:03:54 +0000

    more work

--- verbal memory/verbalmemory.js
+++ verbal memory/verbalmemory.js
@@ -1,23 +1,42 @@
-
 let start = document.getElementById("start");
 let life = document.getElementById("life");
 let score = document.getElementById("score");
 let word = document.getElementById("word");
-let seenword = document.getElementById("seen");
-let newword = document.getElementById("new");
-let wordArray=[];
+let seenbuton = document.getElementById("seen");
+let newbutton = document.getElementById("new");
+let wordArray = [];
+let seenwordArray = [];
 
+//shuffle function
+function shuffle(array) {
+  for (i = 0; i <= array.length - 1; i++) {
+    let randonNumber = Math.floor(Math.random() * (i + 1));
+    let valueAtRandomIndex = array[randonNumber];
+    array[randonNumber] = array[i];
+    array[i] = valueAtRandomIndex;
+  }
+  return array;
+}
 
-start.addEventListener("click",fetchwords,true);
+start.addEventListener("click", fetchwords, true);
 
-function fetchwords(e){
-    e.preventDefault();
-    fetch("https://random-word-api.herokuapp.com/word?number=100")
-    .then((response)=>{
-        return response.json()
+function fetchwords(e) {
+  e.preventDefault();
+  fetch("https://random-word-api.herokuapp.com/word?number=100")
+    .then((response) => {
+      return response.json();
     })
-    .then((data)=>{
-        wordArray=data;
-        console.log(wordArray);
+    .then((data) => {
+      wordArray = data;
+      console.log(wordArray);
+      displaywords();
     })
+    .catch((error) => {
+      alert(error);
+    });
+}
+
+function displaywords() {
+  shuffle(wordArray);
+  word.innerHTML = `<h1>${wordArray[0]}</h1>`;
 }
