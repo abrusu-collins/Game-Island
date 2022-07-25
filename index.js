d5faaff957ec110b77ce66db13b21ab16fc06fdf
abrusu-collins <snillochemingson@gmail.com>
2022-07-25 13:32:16 +0000

    Preloader works

--- index.js
+++ index.js
@@ -19,6 +19,16 @@ document.querySelector('.toggler').onclick = () => {
 //Loader 
 
 
-function loadfunction(){
-  loader.style.display="none";
-}
+// function loadfunction(){
+//   loader.style.display="none";
+// }
+
+$(document).ready(function() {
+  //Preloader
+  preloaderFadeOutTime = 500;
+  function hidePreloader() {
+  var preloader = $('.loader');
+  preloader.fadeOut(preloaderFadeOutTime);
+  }
+  hidePreloader();
+  });
