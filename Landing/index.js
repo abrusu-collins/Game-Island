7249b1d9850bfa6745f048f28e1981858fc8d774
abrusu-collins <snillochemingson@gmail.com>
2022-07-31 12:51:52 +0000

    bug fixed

--- Landing/index.js
+++ Landing/index.js
@@ -13,6 +13,8 @@ window.onscroll = function() {
 
 document.querySelector('.toggler').onclick = () => {
   document.querySelector('.nav').classList.toggle('open');
+  document.querySelector('.nav').classList.toggle('background');
+
 }
 
 
